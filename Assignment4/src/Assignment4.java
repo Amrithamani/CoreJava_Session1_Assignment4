d134c25b05564fd653d5a0d3ae6092ee5f226f04
Amrithamani <amrimani91@gmail.com>
2017-05-25 16:10:23 -0400

    CoreJava_Session1_Assignment4
    CoreJava_Session1_Assignment4 Code

---
+++ Assignment4/src/Assignment4.java
@@ -0,0 +1,36 @@
+
+public class Assignment4 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println( "65-" + (char) 65);
+		System.out.println( "66-" + (char) 66);
+		System.out.println( "67-" + (char) 67);
+		System.out.println( "68-" + (char) 68);
+		System.out.println( "69-" + (char) 69);
+		System.out.println( "70-" + (char) 70);
+		System.out.println( "71-" + (char) 71);
+		System.out.println( "72-" + (char) 72);
+		System.out.println( "73-" + (char) 73);
+		System.out.println( "74-" + (char) 74);
+		System.out.println( "75-" + (char) 75);
+		System.out.println( "76-" + (char) 76);
+		System.out.println( "77-" + (char) 77);
+		System.out.println( "78-" + (char) 78);
+		System.out.println( "79-" + (char) 79);
+		System.out.println( "80-" + (char) 80);
+		System.out.println( "81-" + (char) 81);
+		System.out.println( "82-" + (char) 82);
+		System.out.println( "83-" + (char) 83);
+		System.out.println( "84-" + (char) 84);
+		System.out.println( "85-" + (char) 85);
+		System.out.println( "86-" + (char) 86);
+		System.out.println( "87-" + (char) 87);
+		System.out.println( "88-" + (char) 88);
+		System.out.println( "89-" + (char) 89);
+		System.out.println( "90-" + (char) 90);
+		
+	}
+
+}
